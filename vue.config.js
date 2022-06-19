3c368ff11790bbb5f5102af5a65590f8613338ea
HZchenguanxi <953659450@qq.com>
2022-06-19 16:16:12 +0800

    源文件

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,15 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  transpileDependencies: true,
+  // 关闭lintOnSave
+  lintOnSave: false,
+  // 写原生webpack的配置项
+  configureWebpack: { 
+        devServer: { 
+          open: true,
+          port: 3000,
+          // 更改端口号，localhost,或者127.0.0.1
+          host:'localhost',
+        }
+  }
+});
