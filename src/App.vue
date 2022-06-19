15eafe82c675bba7707b831b4c1f955b7864e685
HZchenguanxi <953659450@qq.com>
2022-06-19 16:49:10 +0800

    完成了帅哥美女走一走作业

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,30 @@
 <template>
-  
+  <div>
+    <ul>
+     <li v-for="item in arr" :key="item">
+       {{item}}
+     </li>
+    </ul>
+    <button @click="btn">走一走</button>
+  </div>
 </template>
 
 <script>
 export default {
-
+ data() {
+   return {
+     arr:['帅哥','美女','程序媛']
+   }
+ },
+  methods: {
+    btn(){
+      // console.log(111);
+      // 吧头部数据加入到末尾
+      this.arr.push(this.arr[0])
+      // 在吧头部的数据删除掉
+      this.arr.shift()
+    }
+  }
 }
 </script>
 
